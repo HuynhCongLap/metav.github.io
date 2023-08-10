a1ce846d2527a151594c81646818fba8866566bd
Cong-Lap <cong-lap@metav.rs>
2023-08-10 11:21:42 +0200

    Updates

--- characterControls.js
+++ characterControls.js
@@ -16,9 +16,10 @@ export class CharacterControls {
         this.currentAction = currentAction;
         this.walkDirection = new THREE.Vector3();
         this.rotateAngle = new THREE.Vector3(0, 1, 0);
+        this.virtualModelPosition = new THREE.Vector3();
         this.rotateQuarternion = new THREE.Quaternion();
         this.cameraTarget = new THREE.Vector3();
-
+        this.bindCamera = true
         this.animationsMap.forEach((value, key) => {
             if (key == currentAction) {
                 value.play();
@@ -32,58 +33,71 @@ export class CharacterControls {
         this.toggleRun = !this.toggleRun;
     }
 
+    freeCamera() {
+        this.bindCamera = !this.bindCamera;
+        console.log("Free Camera !!");
+    }
+
+    getBindCamera() {
+        return this.bindCamera;
+    }
+
     update(delta, keysPressed) {
-        const directionPressed = DIRECTIONS.some(key => keysPressed[key] == true);
+        if(this.bindCamera) {
+            const directionPressed = DIRECTIONS.some(key => keysPressed[key] == true);
 
-        var play = '';
-        if (directionPressed && this.toggleRun) {
-            play = 'Run';
-        } else if (directionPressed) {
-            play = 'Walk';
-        } else {
-            play = 'Idle';
-        }
+            var play = '';
+            if (directionPressed && this.toggleRun) {
+                play = 'Run';
+            } else if (directionPressed) {
+                play = 'Walk';
+            } else {
+                play = 'Idle';
+            }
 
-        if (this.currentAction != play) {
-            const toPlay = this.animationsMap.get(play);
-            const current = this.animationsMap.get(this.currentAction);
+            if (this.currentAction != play) {
+                const toPlay = this.animationsMap.get(play);
+                const current = this.animationsMap.get(this.currentAction);
 
-            current.fadeOut(this.fadeDuration);
-            toPlay.reset().fadeIn(this.fadeDuration).play();
+                current.fadeOut(this.fadeDuration);
+                toPlay.reset().fadeIn(this.fadeDuration).play();
 
-            this.currentAction = play;
-        }
+                this.currentAction = play;
+            }
 
-        this.mixer.update(delta);
+            this.mixer.update(delta);
 
-        if (this.currentAction == 'Run' || this.currentAction == 'Walk') {
-            // calculate towards camera direction
-            var angleYCameraDirection = Math.atan2(
-                this.camera.position.x - this.model.position.x,
-                this.camera.position.z - this.model.position.z
-            );
-            // diagonal movement angle offset
-            var directionOffset = this.directionOffset(keysPressed);
+            if (this.currentAction == 'Run' || this.currentAction == 'Walk') {
+                // calculate towards camera direction
+                var angleYCameraDirection = Math.atan2(
+                    this.camera.position.x - this.model.position.x,
+                    this.camera.position.z - this.model.position.z
+                );
+                // diagonal movement angle offset
+                var directionOffset = this.directionOffset(keysPressed);
 
-            // rotate model
-            this.rotateQuarternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + directionOffset);
-            this.model.quaternion.rotateTowards(this.rotateQuarternion, 0.2);
+                // rotate model
+                this.rotateQuarternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + directionOffset);
+                this.model.quaternion.rotateTowards(this.rotateQuarternion, 0.2);
 
-            // calculate direction
-            this.camera.getWorldDirection(this.walkDirection);
-            this.walkDirection.y = 0;
-            this.walkDirection.normalize();
-            this.walkDirection.applyAxisAngle(this.rotateAngle, directionOffset);
+                // calculate direction
+                this.camera.getWorldDirection(this.walkDirection);
+                this.walkDirection.y = 0;
+                this.walkDirection.normalize();
+                this.walkDirection.applyAxisAngle(this.rotateAngle, directionOffset);
 
-            // run/walk velocity
-            const velocity = this.currentAction == 'Run' ? this.runVelocity : this.walkVelocity;
+                // run/walk velocity
+                const velocity = this.currentAction == 'Run' ? this.runVelocity : this.walkVelocity;
 
-            // move model & camera
-            const moveX = this.walkDirection.x * velocity * delta;
-            const moveZ = this.walkDirection.z * velocity * delta;
-            this.model.position.x += moveX;
-            this.model.position.z += moveZ;
-            this.updateCameraTarget(moveX, moveZ);
+                // move model & camera
+                const moveX = this.walkDirection.x * velocity * delta;
+                const moveZ = this.walkDirection.z * velocity * delta;
+                
+                this.model.position.x += moveX;
+                this.model.position.z += moveZ;
+                this.updateCameraTarget(moveX, moveZ);
+            
+            }
         }
     }
 
@@ -94,7 +108,7 @@ export class CharacterControls {
 
         // update camera target
         this.cameraTarget.x = this.model.position.x;
-        this.cameraTarget.y = this.model.position.y + 1;
+        this.cameraTarget.y = this.model.position.y + 1.5;
         this.cameraTarget.z = this.model.position.z;
         this.orbitControl.target = this.cameraTarget;
     }
